7a1c8acffec6de2c74fea09d24a6b6a0816ca95a
Lê Hiền Lương <75094831+luongic@users.noreply.github.com>
2022-01-03 22:12:13 +0700

    add project
    new project

---
+++ FileExplorer/FileExplorer/Form1.Designer.cs
@@ -0,0 +1,345 @@
+﻿
+namespace FileExplorer
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pathDir = new System.Windows.Forms.TextBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.diskInfoPanel = new System.Windows.Forms.Panel();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.treeNode = new System.Windows.Forms.TreeView();
+            this.listDir = new System.Windows.Forms.ListView();
+            this.columnHeader1 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader2 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader3 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader4 = new System.Windows.Forms.ColumnHeader();
+            this.button5 = new System.Windows.Forms.Button();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.tabControl1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Controls.Add(this.tabPage4);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(908, 81);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 29);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(900, 48);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "File";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 29);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(900, 48);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Computer";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 29);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(900, 48);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "View";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Location = new System.Drawing.Point(4, 29);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage4.Size = new System.Drawing.Size(900, 48);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "Search";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.pathDir);
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 81);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(908, 32);
+            this.panel1.TabIndex = 1;
+            // 
+            // pathDir
+            // 
+            this.pathDir.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pathDir.Location = new System.Drawing.Point(193, 0);
+            this.pathDir.Name = "pathDir";
+            this.pathDir.Size = new System.Drawing.Size(618, 27);
+            this.pathDir.TabIndex = 2;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.button1);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel3.Location = new System.Drawing.Point(811, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(97, 32);
+            this.panel3.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(97, 32);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Copy Path";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.panel4);
+            this.panel2.Controls.Add(this.button5);
+            this.panel2.Controls.Add(this.button3);
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(193, 32);
+            this.panel2.TabIndex = 0;
+            // 
+            // button4
+            // 
+            this.button4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button4.Location = new System.Drawing.Point(0, 0);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(56, 32);
+            this.button4.TabIndex = 2;
+            this.button4.Text = "up";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Dock = System.Windows.Forms.DockStyle.Left;
+            this.button3.Location = new System.Drawing.Point(36, 0);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(39, 32);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "right";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.button2.Location = new System.Drawing.Point(0, 0);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(36, 32);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "left";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // diskInfoPanel
+            // 
+            this.diskInfoPanel.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.diskInfoPanel.Location = new System.Drawing.Point(0, 478);
+            this.diskInfoPanel.Name = "diskInfoPanel";
+            this.diskInfoPanel.Size = new System.Drawing.Size(908, 24);
+            this.diskInfoPanel.TabIndex = 2;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 113);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.treeNode);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.listDir);
+            this.splitContainer1.Size = new System.Drawing.Size(908, 365);
+            this.splitContainer1.SplitterDistance = 302;
+            this.splitContainer1.TabIndex = 3;
+            // 
+            // treeNode
+            // 
+            this.treeNode.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeNode.Location = new System.Drawing.Point(0, 0);
+            this.treeNode.Name = "treeNode";
+            this.treeNode.Size = new System.Drawing.Size(302, 365);
+            this.treeNode.TabIndex = 0;
+            this.treeNode.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeNode_AfterSelect);
+            // 
+            // listDir
+            // 
+            this.listDir.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.listDir.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listDir.HideSelection = false;
+            this.listDir.LabelEdit = true;
+            this.listDir.Location = new System.Drawing.Point(0, 0);
+            this.listDir.Name = "listDir";
+            this.listDir.Size = new System.Drawing.Size(602, 365);
+            this.listDir.TabIndex = 3;
+            this.listDir.UseCompatibleStateImageBehavior = false;
+            this.listDir.View = System.Windows.Forms.View.Details;
+            this.listDir.ItemActivate += new System.EventHandler(this.listDir_ItemActivate);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Name";
+            this.columnHeader1.Width = 200;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Size";
+            this.columnHeader2.Width = 80;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Type";
+            this.columnHeader3.Width = 80;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Date";
+            this.columnHeader4.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.columnHeader4.Width = 80;
+            // 
+            // button5
+            // 
+            this.button5.Dock = System.Windows.Forms.DockStyle.Right;
+            this.button5.Location = new System.Drawing.Point(131, 0);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(62, 32);
+            this.button5.TabIndex = 3;
+            this.button5.Text = "reFresh";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.button4);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel4.Location = new System.Drawing.Point(75, 0);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(56, 32);
+            this.panel4.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(908, 502);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.diskInfoPanel);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "File Explorer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox pathDir;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel diskInfoPanel;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TreeView treeNode;
+        private System.Windows.Forms.ListView listDir;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ FileExplorer/FileExplorer/Form1.cs
@@ -0,0 +1,313 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileExplorer
+{
+    public partial class Form1 : Form
+    {
+        DirectoryInfo curDir; //currentDirrect
+        TreeNode curNode; //currentNode
+
+        bool flag1 = true; //flagCheck
+        bool flagLarge = false;
+
+        List<string> ContentFile = new List<string>();
+        List<string> ContentName = new List<string>();
+
+        List<DirectoryInfo> dsNext = new List<DirectoryInfo>(); //Stack
+        List<DirectoryInfo> dsBack = new List<DirectoryInfo>();
+
+        List<string> list = new List<string>(); //1030
+        List<string> FileName = new List<string>(); //1237 1248 1263
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            InitTreeView();
+        }
+
+        private void InitTreeView() // Khởi tạo TreeView
+        {
+            //Thêm vào danh sách icon
+            treeNode.ImageList = new ImageList();
+            treeNode.ImageList.Images.Add(new Icon("icons/mycomputer.ico"));
+            treeNode.ImageList.Images.Add(new Icon("icons/drive.ico"));
+            treeNode.ImageList.Images.Add(new Icon("icons/folder_close.ico"));
+            treeNode.ImageList.Images.Add(new Icon("icons/folder_open.ico"));
+            treeNode.ImageList.Images.Add(new Icon("icons/document.ico"));
+
+            TreeNode myComputerNode = new TreeNode("This PC");
+            myComputerNode.Tag = "This PC";
+            myComputerNode.ImageIndex = 0;
+            myComputerNode.SelectedImageIndex = 0;
+            treeNode.Nodes.Add(myComputerNode);
+            foreach (DriveInfo drive in DriveInfo.GetDrives())
+            {
+                if (drive.IsReady)
+                {
+                    TreeNode driveNode = new TreeNode(drive.Name);
+                    driveNode.Tag = drive.RootDirectory;
+                    driveNode.ImageIndex = 1;
+                    driveNode.SelectedImageIndex = 1;
+                    myComputerNode.Nodes.Add(driveNode);
+                }
+            }
+        }
+
+        private void treeNode_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            TreeNode selectedNode = treeNode.SelectedNode;
+            curNode = selectedNode;
+            try
+            {
+                // Nếu click vào folder trên TreeVew
+                if (selectedNode.Tag.GetType() == typeof(DirectoryInfo))
+                {
+                    selectedNode.Nodes.Clear();
+                    DirectoryInfo dir = (DirectoryInfo)selectedNode.Tag;
+                    // Lấy danh sách các folder con của folder được click vào
+                    foreach (DirectoryInfo subDir in dir.GetDirectories())
+                    {
+                        // Ẩn file và hiện ra danh sách folder con trên treevew
+                        if ((subDir.Attributes & FileAttributes.Hidden) == FileAttributes.Hidden ||
+                            (subDir.Attributes & FileAttributes.System) == FileAttributes.System)
+                        {
+                            continue;
+                        }
+                        TreeNode dirNode = new TreeNode(subDir.Name);
+                        dirNode.Tag = subDir;
+                        dirNode.ImageIndex = 2;
+                        dirNode.SelectedImageIndex = 3;
+                        selectedNode.Nodes.Add(dirNode);
+                    }
+                    // Hiển thị danh sách folder qua listView
+                    curDir = dir;
+                    pathDir.Text = curDir.FullName;
+                    dsBack.Add(curDir);
+                    LoadDirectory(); //567
+                }
+            }
+            catch
+            {
+                return;
+            }
+        }
+
+        private void LoadDirectory() //
+        {
+            // them danh icon cho listview
+            pathDir.Text = curDir.FullName; //textBox Path
+            if (flagLarge == true) //Large Icon View
+            {
+                ImageList iList = new ImageList();
+                iList.ImageSize = new System.Drawing.Size(64, 64);
+                iList.Images.Add(new Icon("icons/folder_open.ico"));
+                iList.Images.Add(new Icon("icons/document.ico"));
+                iList.Images.Add(new Icon("icons/doc.ico"));//2
+                iList.Images.Add(new Icon("icons/exe.ico"));//3
+                iList.Images.Add(new Icon("icons/img.ico"));//4
+                iList.Images.Add(new Icon("icons/pdf.ico"));//5
+                iList.Images.Add(new Icon("icons/pptx.ico"));//6
+                iList.Images.Add(new Icon("icons/rar.ico"));//7
+                iList.Images.Add(new Icon("icons/sql.ico"));//8
+                iList.Images.Add(new Icon("icons/xls.ico"));//9
+                iList.Images.Add(new Icon("icons/zip.ico"));//10
+                iList.Images.Add(new Icon("icons/unknown.ico"));//11
+                iList.Images.Add(new Icon("icons/clip.ico"));//12
+                listDir.LargeImageList = iList;
+            }
+            else //Small List View
+            {
+                ImageList iList = new ImageList();
+                iList.Images.Add(new Icon("icons/folder_open.ico"));
+                iList.Images.Add(new Icon("icons/document.ico"));
+                iList.Images.Add(new Icon("icons/doc.ico"));//2
+                iList.Images.Add(new Icon("icons/exe.ico"));//3
+                iList.Images.Add(new Icon("icons/img.ico"));//4
+                iList.Images.Add(new Icon("icons/pdf.ico"));//5
+                iList.Images.Add(new Icon("icons/pptx.ico"));//6
+                iList.Images.Add(new Icon("icons/rar.ico"));//7
+                iList.Images.Add(new Icon("icons/sql.ico"));//8
+                iList.Images.Add(new Icon("icons/xls.ico"));//9
+                iList.Images.Add(new Icon("icons/zip.ico"));//10
+                iList.Images.Add(new Icon("icons/unknown.ico"));//11
+                iList.Images.Add(new Icon("icons/clip.ico"));//12
+                listDir.SmallImageList = iList;
+            }
+            listDir.Items.Clear();
+
+            foreach (DirectoryInfo dir in curDir.GetDirectories())
+            {
+                if (((dir.Attributes & FileAttributes.Hidden) == FileAttributes.Hidden) & true == flag1)//flag 1 cho hidden file
+                {
+                    continue;
+                }
+                if ((dir.Attributes & FileAttributes.System) == FileAttributes.System)
+                {
+                    continue;
+                }
+                ListViewItem lvi = listDir.Items.Add(dir.Name);
+                lvi.Tag = dir;
+                lvi.ImageIndex = 0;
+                lvi.SubItems.Add("");
+                lvi.SubItems.Add("Folder");
+                lvi.SubItems.Add(dir.LastWriteTime.ToString());
+            }
+            foreach (FileInfo file in curDir.GetFiles())
+            {
+                if ((file.Attributes & FileAttributes.Hidden) == FileAttributes.Hidden)
+                {
+                    continue;
+                }
+                if ((file.Attributes & FileAttributes.System) == FileAttributes.System)
+                {
+                    continue;
+                }
+
+                loadType(file);
+
+                int typeFile;
+
+                switch (file.Extension.ToUpper())
+                {
+
+                    case ".DOC":
+                    case ".DOCX":
+                        typeFile = 2;
+                        break;
+
+                    case ".EXE":
+                        typeFile = 3;
+                        break;
+
+                    case ".PDF":
+                        typeFile = 5;
+                        break;
+
+                    case ".PPTX":
+                    case ".PTX":
+                        typeFile = 6;
+                        break;
+
+                    case ".RAR":
+                        typeFile = 7;
+                        break;
+
+                    case ".SQL":
+                        typeFile = 8;
+                        break;
+
+                    case ".XLS":
+                        typeFile = 9;
+                        break;
+
+                    case ".ZIP":
+                        typeFile = 10;
+                        break;
+
+                    case ".MP3":
+                    case ".MP4":
+                    case ".WMV":
+                        typeFile = 12;
+                        break;
+
+                    case ".PNG":
+                    case ".JPG":
+                        typeFile = 4;
+                        break;
+                    default:
+                        typeFile = 11;
+                        break;
+                }
+                ListViewItem listItem = listDir.Items.Add(file.Name);
+                listItem.Tag = file;
+                listItem.ImageIndex = typeFile;
+                listItem.SubItems.Add(GetSizeStr(file.Length));
+                listItem.SubItems.Add(file.Extension.Substring(1));
+                listItem.SubItems.Add(file.LastWriteTime.ToString());
+            }
+        }
+
+        public void loadType(FileInfo file) //Lấy type của file
+        {
+            string Type = file.Extension.Substring(1);
+            if (list.Contains(Type) == false)
+            {
+                list.Add(Type);
+                //type.Items.Add(Type);
+            }
+        }
+
+        // Format size theo B KB MB GB
+        public static string GetSizeStr(float fileSize)
+        {
+            string fileSizeStr = string.Empty;
+
+            if (fileSize < 1024 * 1024)
+                fileSizeStr = Math.Round(fileSize * 1.0 / 1024, 2) + " KB";
+
+            else if (fileSize >= 1024 * 1024 && fileSize < 1024 * 1024 * 1024)
+                fileSizeStr = Math.Round(fileSize * 1.0 / (1024 * 1024), 2) + " MB";
+
+            else if (fileSize >= 1024 * 1024 * 1024)
+                fileSizeStr = Math.Round(fileSize * 1.0 / (1024 * 1024 * 1024), 2) + " GB";
+
+            return fileSizeStr;
+        }
+        private TreeNode findCurrentNode(string textNode)
+        {
+            int foundIndex = 0;
+            int indexCount = 0;
+            foreach (TreeNode node in curNode.Nodes)
+            {
+                if (node.Text.Equals(textNode))
+                {
+                    foundIndex = indexCount;
+                }
+                indexCount++;
+            }
+            return curNode.Nodes[foundIndex];
+        }
+
+        private void listDir_ItemActivate(object sender, EventArgs e)
+        {
+            try
+            {
+                if (listDir.SelectedItems[0].Tag.GetType() == typeof(DirectoryInfo))
+                {
+                    curDir = (DirectoryInfo)listDir.SelectedItems[0].Tag;
+                    LoadDirectory();
+                    curNode = findCurrentNode(curDir.Name);
+                    treeNode.SelectedNode = curNode;
+                    pathDir.Text = curDir.FullName;
+                }
+                else
+                {
+                    FileInfo file = (FileInfo)listDir.SelectedItems[0].Tag;
+                    Process.Start(file.FullName);
+                }
+            }
+            catch
+            {
+                return;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
